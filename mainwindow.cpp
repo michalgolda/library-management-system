8b03897d62d7f164b73f6ba7adc3192c8aead8c0
goldamichal <michal.golda@ziomax.pl>
2025-12-19 19:18:25 +0100

    chore: create simple sqlite connection

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,11 +1,23 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QSqlDatabase>
+#include <QDebug>
+#include <QDir>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+
+    QString databaseName = QDir::currentPath() + QString("/main.sqlite");
+    QSqlDatabase db = QSqlDatabase::addDatabase("QSQLITE");
+    db.setDatabaseName(databaseName);
+    bool dbIsOpen = db.open();
+
+    qDebug() << "Database open result: " << dbIsOpen;
+    qDebug() << databaseName;
 }
 
 MainWindow::~MainWindow()
